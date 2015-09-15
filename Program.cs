1da8b31bf3b3e93b04d4152b63ff0385e6f0f045
Avantia P&D <marcilio.leite@grupoavantia.com.br>
2015-09-15 09:56:49 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using AvvaCSharpSdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AvvaCSharpSdkExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string host = "http://localhost:61773";
+                string secretKey = "a9bfaef562396c30abed90577ebc00c1";
+
+                SpectatorAlertsClient alertsClient = new SpectatorAlertsClient(
+                    host, 
+                    secretKey
+                );
+
+                alertsClient.OnAlertTriggered += AlertsClient_AlertTriggeredEvent;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            ReadKey();
+        }
+
+        private static void AlertsClient_AlertTriggeredEvent(object sender, 
+            SpectatorAlertEventArgs e)
+        {
+            WriteLine($"{e.CameraName} at {e.CreatedAt}");
+        }
+    }
+}
